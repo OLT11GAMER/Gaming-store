79d95c89ab9dbe580398c4c67ef208df8f9c8273
OLT11GAMER <93716411+OLT11GAMER@users.noreply.github.com>
2024-01-30 19:08:41 +0100

    last push

--- app.js
+++ app.js
@@ -1,62 +1,90 @@
 let express = require('express');
 let app = express();
 let mysql = require('mysql2');
+const bcrypt = require('bcrypt');
 
-const bodyParser = require('body-parser');
-
-// Parse form data
-app.use(bodyParser.urlencoded({ extended: true }));
-
-app.use('/static', express.static('static'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
+app.use('/views', express.static('views'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 app.set('view engine', 'ejs');
+
+
+
 let database = require('./database');
 database.sync();
+
+
 let modeli = require('./userModel');
-let bcrypt = require('bcrypt');
+
+app.get('/', (req, res) => {  
+    res.render('index.ejs');
+})
 
 app.get('/register', (req, res) => {
-    res.render("register.ejs");
+  res.render('register.ejs');
 })
-app.post(
-    '/register', (req, res) => {
-        if(req.body.password == req.body.confirmPassword){
-            // insertoi te dhenat ne databaze
-            let passwordiEnkriptuar = bcrypt.hashSync(req.body.password, 6)
-            console.log(`passworDI JUAJ : ${passwordiEnkriptuar} ..... ESHTE ENKRIPTUAR`)
-            modeli.build({
-                username: req.body.username,
-                email: req.body.email,
-                password: passwordiEnkriptuar 
-            }).save().then(
-                ()=>{res.send('te dhenat u insertuan')})
-                .catch((err)=>{res.send(err)})
-                .then(res.render("index"))
-            // res.send("passwordaat jane okej");
-        } else {
-            res.send("passwordat jane gabim");
-        }
+
+app.post('/register', async (req, res) => {
+
+  try {
+    if (req.body.password === req.body.confirmPassword) {
+
+      let passwordiEnkriptuar = await bcrypt.hash(req.body.password, 6);
+
+      console.log(`Password Hash: ${passwordiEnkriptuar}`);
+
+
+
+
+      await modeli.create({
+        username: req.body.username,
+        email: req.body.email,
+        password: passwordiEnkriptuar,
+      });
+      res.render('index.ejs');
+    } else {
+
+      res.send('passwords do not match');
+
     }
-)
-
-// Create a simple route for the login page
-app.get('/login', (req, res) => {
-  res.render('login');
-});
-// Handle the login form submission
-app.post('/login', (req, res) => {
-  const username = req.body.username;
-  const password = req.body.password;
-
-  // Check the username and password (you can implement your authentication logic here)
-
-  if (username === 'yourusername' && password === 'yourpassword') {
-    // Successful login, you can redirect to another page or perform further actions
-    res.send('Login successful');
-  } else {
-    res.send('Login failed');
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Internal Server Error');
   }
-});
+})
+app.get('/login', (req, res) => {
+  res.render('login.ejs');
+})
 
-app.listen(8081);
+app.post('/login', async (req, res) => {
+  try {
+    const user = await modeli.findOne({ where: { username: req.body.username } });
+    if (!user) {
+      return res.status(404).send('User not found');
+    } else {
+      bcrypt.compare(req.body.password, user.password, (err, resu) => {
+        if (err) {
+          console.log(err);
+        }
+        else {
+          console.log(resu);
+
+          if (resu) {
+            res.render('index.ejs')
+
+
+          } else {
+            res.render('/login.ejs')
+          }
+        }
+
+      })
+    }
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+})
+app.listen(8081, () => {
+  console.log('Server is running on port 8081');
+});
